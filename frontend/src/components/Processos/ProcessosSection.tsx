4f4cc024a90031038b8ac32236f0f19b11af5000
Amaral <victormartinsl0266@gmail.com>
2026-07-28 09:50:34 -0300

    Corrige formatação da data de entrada

--- frontend/src/components/Processos/ProcessosSection.tsx
+++ frontend/src/components/Processos/ProcessosSection.tsx
@@ -67,7 +67,9 @@ function dateOnly(value: string) {
 
 function formatarEntrada(value: string) {
   return new Intl.DateTimeFormat('pt-BR', {
-    dateStyle: 'short',
+    day: '2-digit',
+    month: '2-digit',
+    year: 'numeric',
     hour: '2-digit',
     minute: '2-digit',
   }).format(new Date(value));
